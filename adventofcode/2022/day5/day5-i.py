4b5db0b3963901f2dd195b5ffe5d4232cd673a61
Noah <thenoahleuenberger@gmail.com>
2022-12-05 16:55:28 +0100

    Code cleanup for aoc day 5

--- adventofcode/2022/day5/day5-i.py
+++ adventofcode/2022/day5/day5-i.py
@@ -1,29 +1,32 @@
 with open('input.txt') as f:
     build_stack = True
-    stack_lines = []
-    stacks = None
+    stacks = {}
 
     for line in f:
         line = line.rstrip()
-
         if len(line) == 0:
+            continue
+
+        if build_stack and line.replace(" ", "").isdigit():
+            for k in stacks.keys():
+                stacks[k] = stacks[k][::-1]
+
             build_stack = False
-            stack_lines = stack_lines[::-1]
-
-            count_stacks = int(stack_lines.pop(0)[-1])
-            stacks = {i: [] for i in range(1, count_stacks + 1)}
-
-            for stack_line in stack_lines:
-                for i in stacks.keys():
-                    if len(stack_line) - 1 >= 4 * i - 3:
-                        container = stack_line[4 * i - 3].strip()
-                        if len(container) > 0:
-                            stacks[i].append(container)
-
             continue
 
         if build_stack:
-            stack_lines.append(line)
+            index, i = 1, 1
+            while index <= len(line) - 1:
+                if i not in stacks.keys():
+                    stacks[i] = []
+
+                container = line[index].strip()
+                if len(container) == 1:
+                    stacks[i].append(container)
+
+                i += 1
+                index = 4 * i - 3
+
             continue
 
         command = [int(x) for x in line.split(" ") if x.isdigit()]
